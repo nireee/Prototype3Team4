afb23c02bcdb5ec3169702d575c3a612cd35c443
nireee <u1426029@umail.utah.edu>
2022-10-31 15:43:24 -0600

    click the white pelican to pop out text window

--- Assets/Scripts/Interact.cs
+++ Assets/Scripts/Interact.cs
@@ -6,17 +6,18 @@ public class Interact : MonoBehaviour
 {
     public Camera cam;
     public bool Touched;
+    public GameObject TextPanel;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Touched = false;
+        TextPanel.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         CheckTouchTag();
-        Debug.Log(Touched);
         //Reposition();
     }
 
@@ -27,11 +28,13 @@ public class Interact : MonoBehaviour
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
-            if (Physics.Raycast(ray, out hit))
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
             {
                 if (hit.transform.tag == "InteractObj")
                 {
-                    Touched = !Touched;
+                    Touched = true;
+                    TextPanel.SetActive(Touched);
+                    Touched = false;
                 }
             }
 
